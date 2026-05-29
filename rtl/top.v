aed510c9d0d06c9654f0e34e05305869ef64dfb6
ChidumamAmadi-Obi <amadiobichidumam@gmail.com>
2026-05-29 17:37:53 +0100

    created top module for testing
    top module links rx and tx modules, the fpga will echo the message sent by the pc back to it

--- rtl/top.v
+++ rtl/top.v
@@ -8,10 +8,19 @@ module uart (
     input wire txUart,
 );
 
-reg [7:0] msgReceived [0:MESSAGE_BUFFER_LENGTH-1];
-reg [7:0] msgToSend [0:MESSAGE_BUFFER_LENGTH-1];
-
+reg [7:0] msg [0:MSG_BUFFER_LENGTH-1]; // for now the fpga is just gonna echo received msg back to pc
 wire msgRdy;
 
+rx rxModule(
+    .clk(clk),
+    .rx(rxUart),
+    .rdy(msgRdy),
+    .msgOut(msg)); // msg received by fpga
+
+tx txModule(
+    .clk(clk),
+    .rdy(msgRdy),
+    .tx(txUart),
+    .msg(msg)); // msg fpga is sending
     
 endmodule
