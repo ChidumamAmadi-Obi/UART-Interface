27d990e43169c08e3b55c378b4791a4b9980fc54
ChidumamAmadi-Obi <amadiobichidumam@gmail.com>
2026-09-05 16:40:24 +0100

    verified SPI slave tx

--- tb/spi_slave_tb.sv
+++ tb/spi_slave_tb.sv
@@ -20,13 +20,6 @@ input logic [31:0] data;
         mosi = data[i]; spiToggleSck(sck);
     end
 endtask
-task mcuSend16; // fpga receives from mcu
-ref logic mosi;
-input logic [15:0] data;
-    for (int i=31; i>=0; i--) begin
-        mosi = data[i]; spiToggleSck(sck);
-    end
-endtask
 task mcuSend8; // fpga receives from mcu
 ref logic mosi;
 input logic [7:0] data;
@@ -34,26 +27,31 @@ input logic [7:0] data;
         mosi = data[i]; spiToggleSck(sck);
     end
 endtask
-task mcuSend4; // fpga receives from mcu
-ref logic mosi;
-input logic [7:0] data;
-    for (int i=7; i>=0; i--) begin
-        mosi = data[i]; spiToggleSck(sck);
-    end
-endtask
 
-task fpgaSend32;
+task mcuReceive32;
 ref logic miso;
 ref logic mosi;
 ref logic sck;
-input logic [31:0] data;
-
+output logic [31:0] data;
     for (int i=31; i>=0; i--) begin 
-        sck = 1'b0;
-        mosi = 1'b0; #(SPI_CLK_PERIOD/2);
+        mosi = 1'b0; 
+        sck = 1'b0; #(SPI_CLK_PERIOD);
 
-        sck = 1'b1;
-        data[i] = miso; #(SPI_CLK_PERIOD/2);
+        data[i] = miso; 
+        sck = 1'b1; #(SPI_CLK_PERIOD);
+    end
+endtask
+task mcuReceive8;
+ref logic miso;
+ref logic mosi;
+ref logic sck;
+output logic [7:0] data;
+    for (int i=7; i>=0; i--) begin 
+        mosi = 1'b0; 
+        sck = 1'b0; #(SPI_CLK_PERIOD);
+
+        data[i] = miso; 
+        sck = 1'b1; #(SPI_CLK_PERIOD);
     end
 endtask
 
@@ -73,23 +71,32 @@ always #1 clk = ~clk;
 initial begin 
     clk = 1'b0;
     rstn = 1'b1;
-
     sck = 1'b0;
     mosi = 1'b0;
     csn = 1'b1;
     data_in = 32'b0;
 
-    $monitor("DATA RECEIVED FROM MCU: 0x%0H", spi_slave_topInstance.wordIN);
 
     csn = 1'b0; // csn active
-
+    
     mcuSend8(mosi, 8'h12);
-    mosi = 1'b0;
-    mcuSend32(mosi, 32'hDEADEAD);
-    mosi = 1'b0;
-    
+    $display("MCU SENT: 0x%h, FPGA RECEIVED: 0x%h", 8'h12, spi_slave_topInstance.wordIN);
+    mcuSend32(mosi, 32'hDEADBEEF);
+    $display("MCU SENT: 0x%h, FPGA RECEIVED: 0x%h", 32'hDEADBEEF, spi_slave_topInstance.wordIN);
+
     csn = 1'b1; // csn inactive
+
+    #(10);
     
+    csn = 1'b0;
+
+    data_in = 32'h12345678; // send data to transmit to mcu
+    mcuReceive32(miso, mosi, sck, expectedData_in);
+    $display("FPGA SENT: 0x%h, MCU RECEIVED: 0x%h", 32'h12345678, expectedData_in);
+
+    csn = 1'b1;
+    
+    #(10);
     $finish;
 end
 
